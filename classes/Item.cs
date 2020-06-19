65e26a68d0cc7da3d3c9b881f671c0ce1c34469f
Banshee <minato187@outlook.com>
2020-06-19 23:21:02 +0200

    Created db, Item and Company class

---
+++ classes/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GORIPBS.classes
+{
+    class Item
+    {
+        public string ItemCode { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Item() { }
+
+        public Item(string itemCode, string name, int price) 
+        {
+            this.ItemCode = itemCode;
+            this.Name = name;
+            this.Price = price;
+        }
+    }
+}
