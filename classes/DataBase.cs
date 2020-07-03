59b53fb7164ca3cded8db48ffe740624b17f2607
Banshee <minato187@outlook.com>
2020-07-03 17:00:44 +0200

    Added SQLite driver and EntityFramework

---
+++ classes/DataBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace GORIPBS.classes
+{
+    class DataBase
+    {
+        public static DataBase instance;
+        private SQLiteConnection
+
+        public static DataBase DataBaseSingleton() 
+        {
+            if (instance == null)
+                instance = new DataBase();
+            return instance;
+        }
+
+
+    }
+}
